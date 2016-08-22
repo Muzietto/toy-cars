f8019fcae474ff6ba47300bf0ede0c07d8200b94
Muzietto <marco_faustinelli@yahoo.com>
2016-08-22 19:37:05 +0100

    preprocess ready

--- js/toycars_0.1.js
+++ js/toycars_0.1.js
@@ -9,10 +9,15 @@
 */
 var TC = function(L, V) {
   
-  // e.g. '5,6:FFRLF'
+  // e.g. '5,6:FFR' --> { startFrame: {origin: (5,6), heading: (5,7)}, moves: ('F','F','R')}
   function preprocess(instructions) {
-    var result = {};
-    
+    var raw = instructions.split(':');
+    var digits = raw[0].split(',').map(function(s) { return parseInt(s, 10); });
+    var moves = raw[1].split('');
+    var result = { startFrame: { origin: null, heading: null }, moves: null };
+    result.startFrame.origin = V.make_vect(digits[0], digits[1]);
+    result.startFrame.heading = V.make_vect(digits[0], digits[1]+1);
+    result.moves = L.ArrayToList(moves);
     return result;
   }
 
