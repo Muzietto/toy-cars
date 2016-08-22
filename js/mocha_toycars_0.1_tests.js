4e248d090f4ed643d920f6e664fadc9fc1254306
Muzietto <marco_faustinelli@yahoo.com>
2016-08-22 22:48:30 +0100

    implemented step

--- js/mocha_toycars_0.1_tests.js
+++ js/mocha_toycars_0.1_tests.js
@@ -34,7 +34,14 @@ describe('toycars', function () {
     expect(V.ycor_vect(testFrame.heading)).to.be.equal(3);
   });
 
-  xdescribe('create new frames depending on current frame and moves', function () {
+  describe('create new frames depending on current frame and moves', function () {
+    it ('refusing invalid moves', function() {
+      expect(function() { TC.step('F', TC.frame(V.make_vect(2,4), V.make_vect(2,5)))}).to.not.throw;
+      expect(function() { TC.step('R', TC.frame(V.make_vect(2,4), V.make_vect(2,5)))}).to.not.throw;
+      expect(function() { TC.step('L', TC.frame(V.make_vect(2,4), V.make_vect(2,5)))}).to.not.throw;
+      expect(function() { TC.step('XXX', TC.frame(V.make_vect(2,4), V.make_vect(2,5)))}).to.throw;
+    })
+
     it('starting from an upwards heading', function() {
     var startFrame = TC.frame(V.make_vect(2,4), V.make_vect(2,5));
     var moveFrontFrame = TC.step('F', startFrame);
@@ -42,18 +49,39 @@ describe('toycars', function () {
     expect(V.ycor_vect(moveFrontFrame.origin)).to.be.equal(5);
     expect(V.xcor_vect(moveFrontFrame.heading)).to.be.equal(2);
     expect(V.ycor_vect(moveFrontFrame.heading)).to.be.equal(6);
-    
+
     var moveRightFrame = TC.step('R', startFrame);
     expect(V.xcor_vect(moveRightFrame.origin)).to.be.equal(2);
     expect(V.ycor_vect(moveRightFrame.origin)).to.be.equal(5);
     expect(V.xcor_vect(moveRightFrame.heading)).to.be.equal(3);
     expect(V.ycor_vect(moveRightFrame.heading)).to.be.equal(5);
-    
+
     var moveLeftFrame = TC.step('L', startFrame);
     expect(V.xcor_vect(moveLeftFrame.origin)).to.be.equal(2);
     expect(V.ycor_vect(moveLeftFrame.origin)).to.be.equal(5);
     expect(V.xcor_vect(moveLeftFrame.heading)).to.be.equal(1);
     expect(V.ycor_vect(moveLeftFrame.heading)).to.be.equal(5);
+    });
+
+    it('starting from a downwards heading', function() {
+    var startFrame = TC.frame(V.make_vect(2,4), V.make_vect(2,3));
+    var moveFrontFrame = TC.step('F', startFrame);
+    expect(V.xcor_vect(moveFrontFrame.origin)).to.be.equal(2);
+    expect(V.ycor_vect(moveFrontFrame.origin)).to.be.equal(3);
+    expect(V.xcor_vect(moveFrontFrame.heading)).to.be.equal(2);
+    expect(V.ycor_vect(moveFrontFrame.heading)).to.be.equal(2);
+
+    var moveRightFrame = TC.step('R', startFrame);
+    expect(V.xcor_vect(moveRightFrame.origin)).to.be.equal(2);
+    expect(V.ycor_vect(moveRightFrame.origin)).to.be.equal(3);
+    expect(V.xcor_vect(moveRightFrame.heading)).to.be.equal(1);
+    expect(V.ycor_vect(moveRightFrame.heading)).to.be.equal(3);
+
+    var moveLeftFrame = TC.step('L', startFrame);
+    expect(V.xcor_vect(moveLeftFrame.origin)).to.be.equal(2);
+    expect(V.ycor_vect(moveLeftFrame.origin)).to.be.equal(3);
+    expect(V.xcor_vect(moveLeftFrame.heading)).to.be.equal(3);
+    expect(V.ycor_vect(moveLeftFrame.heading)).to.be.equal(3);
     });
 
     it('starting from a rightwards heading, and so on...', function() {
@@ -63,13 +91,13 @@ describe('toycars', function () {
     expect(V.ycor_vect(moveFrontFrame.origin)).to.be.equal(4);
     expect(V.xcor_vect(moveFrontFrame.heading)).to.be.equal(4);
     expect(V.ycor_vect(moveFrontFrame.heading)).to.be.equal(4);
-    
+
     var moveRightFrame = TC.step('R', startFrame);
     expect(V.xcor_vect(moveRightFrame.origin)).to.be.equal(3);
     expect(V.ycor_vect(moveRightFrame.origin)).to.be.equal(4);
     expect(V.xcor_vect(moveRightFrame.heading)).to.be.equal(3);
     expect(V.ycor_vect(moveRightFrame.heading)).to.be.equal(3);
-    
+
     var moveLeftFrame = TC.step('L', startFrame);
     expect(V.xcor_vect(moveLeftFrame.origin)).to.be.equal(3);
     expect(V.ycor_vect(moveLeftFrame.origin)).to.be.equal(4);
--- js/toycars_0.1.js
+++ js/toycars_0.1.js
@@ -8,7 +8,7 @@
 	The Apache 2.0 License - Copyright (c) 2016 Toycars Project
 */
 var TC = function(L, V) {
-  
+
   // e.g. '5,6:FFR' --> { startFrame: {origin: (5,6), heading: (5,7)}, moves: ('F','F','R')}
   function preprocess(instructions) {
     var raw = instructions.split(':');
@@ -20,13 +20,37 @@ var TC = function(L, V) {
     result.moves = L.ArrayToList(moves);
     return result;
   }
-  
+
   function frame(origin, heading) {
     return { origin: origin, heading: heading };
   }
 
+  function step(move, frame) {
+    var F = V.make_vect(1, 0);
+    var R = V.make_vect(0, -1);
+    var L = V.make_vect(0, 1);
+    var result = {};
+    result.origin = frame.heading;
+    var versor = V.sub_vect(frame.heading, frame.origin);
+    switch(move) {
+      case 'F':
+        result.heading = V.add_vect(frame.heading, V.rotate_vect(F, versor));
+        break;
+      case 'R':
+        result.heading = V.add_vect(frame.heading, V.rotate_vect(R, versor));
+        break;
+      case 'L':
+        result.heading = V.add_vect(frame.heading, V.rotate_vect(L, versor));
+        break;
+      default:
+        throw new Error('invalid move');
+    }
+    return result;
+  }
+
   return {
     preprocess: preprocess,
-    frame: frame
+    frame: frame,
+    step: step
   };
 }(geieslists, geiesvectors);
